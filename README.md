bc1c56311310affd728fd48153ee9300696dbbdb
Balthazard <balthazard.grego@epitech.eu>
2021-03-14 19:29:26 +0100

    Readme Updated

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # EPI_JAM_2020_2
 48h Epitech Jam 2nd edition. [THEME: STREET ART]
 
+# Streetline Preview
+### Main Menu
+![Streetline](Screenshots/Streetshot_00.png)
+
+### Play Mode 
+![Streetline](StreetlinePrev.gif)
+
+### Creator Mode 
+![Streetline](StreetlinePrev2.gif)
+
+### StreetShots
+![Streetline](Screenshots/perroquet.png)
+
+![Streetline](Screenshots/ghostMario2.png)
+
+![Streetline](Screenshots/EmojiLove.png)
+
+![Streetline](Screenshots/Mario.png)
+
+![Streetline](Screenshots/Nemo.png)
+
+![Streetline](Screenshots/bruni.png)
+
+![Streetline](Screenshots/StreetlineShot.png)
+
+![Streetline](Screenshots/StreetlineShot_2.png)
+
+
+
 *HOW TO PLAY :*
 # Linux
--   Open your terminal in 'EPI_JAM_2020_2/bin/Linux' and run './StreetLineLNX.x86_64'
+-   Open your terminal in the root directory (EPI_JAM_2020_2) and run `cd bin/Linux && ./StreetLineLNX.x86_64`
 # Windows
 -   Open folder 'EPI_JAM_2020_2/bin/Windows' and run 'StreetLine.exe'
 
--- StreetLine/Assets/Scripts/colorAction.cs
+++ StreetLine/Assets/Scripts/colorAction.cs
@@ -6,14 +6,30 @@ public class colorAction : MonoBehaviour
 {
     private Shapes2D.Shape shape;
     private SpriteRenderer myRenderer;
+    BoxCollider2D m_Collider;
     // private Color [] possibleColors = new Color[10];
     // Start is called before the first frame update
     void Awake()
     {
         shape = GetComponent<Shapes2D.Shape>();
         myRenderer = GetComponent<SpriteRenderer>();
+        m_Collider = GetComponent<BoxCollider2D>();
         // shape.settings.fillColor = cellColor;
     }
+
+    // void Update()
+    // {
+    //     Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //      Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+    //     Debug.Log(mousePosition);
+    //     Debug.Log(pz);
+
+    //     if (m_Collider.bounds.Contains(mousePosition))
+    //     {
+    //         Debug.Log("Bounds contain the point : " + mousePosition);
+    //     }
+    // }
     private void OnTriggerEnter2D(Collider2D other)
     {
         #if UNITY_ANDROID
